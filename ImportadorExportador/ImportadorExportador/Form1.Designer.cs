4f820c1dce1cd4d9cc1647ab3597e0adad350adf
Rafael de Araújo Andrade <rafael.araujo.andrade@gmail.com>
2023-02-28 11:49:49 -0300

    Feito a parte de Importação

---
+++ ImportadorExportador/ImportadorExportador/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ImportadorExportador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnImportar = new System.Windows.Forms.Button();
+            this.txtArquivoImportar = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnImportar
+            // 
+            this.btnImportar.Location = new System.Drawing.Point(341, 19);
+            this.btnImportar.Name = "btnImportar";
+            this.btnImportar.Size = new System.Drawing.Size(75, 29);
+            this.btnImportar.TabIndex = 0;
+            this.btnImportar.Text = "Importar";
+            this.btnImportar.UseVisualStyleBackColor = true;
+            this.btnImportar.Click += new System.EventHandler(this.btnImportar_Click);
+            // 
+            // txtArquivoImportar
+            // 
+            this.txtArquivoImportar.Location = new System.Drawing.Point(24, 22);
+            this.txtArquivoImportar.Name = "txtArquivoImportar";
+            this.txtArquivoImportar.Size = new System.Drawing.Size(311, 22);
+            this.txtArquivoImportar.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtArquivoImportar);
+            this.Controls.Add(this.btnImportar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImportar;
+        private System.Windows.Forms.TextBox txtArquivoImportar;
+    }
+}
+
---
+++ ImportadorExportador/ImportadorExportador/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ImportadorExportador
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnImportar_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(txtArquivoImportar.Text) == false)
+            {
+                MessageBox.Show("Arquivo informado não foi encontrado!", "Erro");
+                return;
+            }
+
+            /*[
+	            {
+		            "id": 10,
+		            "codEstabel": "2",
+		            "NrPedCli": "200500",
+		            "itens": [{
+				            "seq": 10,
+				            "codItem": "ATL1020",
+				            "qtde": 10,
+				            "precoUnit": 10.50,
+				            "total": 105
+			            }, {
+				            "seq": 20,
+				            "codItem": "ATL2580",
+				            "qtde": 100,
+				            "precoUnit": 2.50,
+				            "total": 250
+			            }
+		            ]
+	            }
+            ]*/
+
+            String conteudoArquivo = File.ReadAllText(txtArquivoImportar.Text);
+
+            MessageBox.Show(conteudoArquivo);
+
+            JArray jArrayPedidos = JArray.Parse(conteudoArquivo);
+            JArray jArrayItens;
+
+            foreach (JObject jObjectPedido in jArrayPedidos)
+            {
+                MessageBox.Show(jObjectPedido.GetValue("id").ToString());
+                if (jObjectPedido.ContainsKey("itens"))
+                {
+                    jArrayItens = JArray.FromObject(jObjectPedido.GetValue("itens"));
+
+                    foreach (JObject jObjectItem in jArrayItens)
+                    {
+                        MessageBox.Show(getValor(jObjectItem,"seq") + Environment.NewLine +
+                                        getValor(jObjectItem,"codItem") + Environment.NewLine +
+                                        getValor(jObjectItem,"qtde") + Environment.NewLine +
+                                        getValor(jObjectItem,"precoUnit") + Environment.NewLine +
+                                        getValor(jObjectItem,"total"));
+                    }
+                }
+            }
+        }
+
+        private object getValor(JObject jObjeto,String campo)
+        {
+            if (jObjeto.ContainsKey(campo))
+            {
+                return jObjeto.GetValue(campo);
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
